49504d4d5c06594689f2947208fc60690ec67a7d
TALLISON <tallison@apache.org>
2019-05-31 09:09:23 -0400

    Add average docs returned and average elapsed time - this fixes #51.

---
+++ quaerite-core/src/main/java/org/mitre/quaerite/core/scorers/AverageElapsedTime.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.mitre.quaerite.core.scorers;
+
+import org.mitre.quaerite.core.QueryInfo;
+import org.mitre.quaerite.core.SearchResultSet;
+
+public class AverageElapsedTime extends DistributionalScoreAggregator
+    implements SearchResultSetScorer {
+
+    public AverageElapsedTime(int atN) {
+        super("AverageElapsedTime", atN);
+    }
+
+    @Override
+    public double score(QueryInfo queryInfo, SearchResultSet searchResultSet) {
+        double elapsed = searchResultSet.getElapsedTime();
+        addScore(queryInfo, elapsed);
+        return elapsed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AverageElapsedTime)) return false;
+        return super.equals(o);
+    }
+}
